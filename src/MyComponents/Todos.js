5931d490bfb54d68153f397289123fe68b6e65ab
Arpan Raval <arpanraval1112@gmail.com>
2024-08-31 01:31:52 +0530

    todo items

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { TodoItem } from './TodoItem'
 
-export const Todos = () => {
+export const Todos = (props) => {
   return (
-    <div>Todos</div>
+    <div className="container">
+      <h3 className="text-center my-3">Todo List</h3>
+      {
+        props.todos.map((todo)=>{
+          return <TodoItem todo={todo} key={todo.number} onDelete={props.onDelete}/>
+        })
+      }
+    </div>
   )
 }
