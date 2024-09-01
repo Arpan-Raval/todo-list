e223c3cd83080433b4935cae8484f381eca9a63a
Arpan Raval <arpanraval1112@gmail.com>
2024-09-01 11:40:22 +0530

    change name

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -9,7 +9,7 @@ export const Todos = (props) => {
     return (
         <div className="container" style={myStyle}>
             <h3 className="my-3">Todos List</h3>
-            {props.todos.length===0? "No Todos to display":  
+            {props.todos.length===0? "Empty Todos":  
             props.todos.map((todo)=>{
                 console.log(todo.sno);
                 return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>   
